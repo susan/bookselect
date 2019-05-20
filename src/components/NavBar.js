7a64a7976a73ab37338a615d9b545b54e14ea633
susan <susan@users.noreply.github.com>
2019-05-20 16:06:15 -0400

    type

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,6 @@ import { connect } from "react-redux";
 import { logoutUser} from "../actions/userAction";
 import { clearCart } from "../actions/cartAction";
 
-
 class NavBar extends Component {
 
   logout = () => {
@@ -18,7 +17,7 @@ class NavBar extends Component {
    render() {
      return (
         <div className = "nav">
-          <NavLink className = "nav_item" to="/reports"> Reports  </NavLink>
+          <NavLink className = "nav_item" to="/books"> Books  </NavLink>
           <NavLink className = "nav_item" to="/register"> Sign Up </NavLink>
           <NavLink className = "nav_item" to="/login"> Login </NavLink>
           <NavLink className = "nav_item" onClick={() => this.logout()} to="/books"> Logout </NavLink>
